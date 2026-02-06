eb1dcfaccd7c8f3aaf5393acbf4a317957facc4a
moncef dante <dante2410@hotmail.fr>
2026-02-06 17:07:49 +0100

    first commit

---
+++ app/Models/Item.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Item extends Model
+{
+    /** @use HasFactory<\Database\Factories\ItemFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'sku',
+        'price',
+        'status',
+    ];
+
+    public function inventory(): HasOne
+    {
+        return $this->hasOne(Inventory::class);
+    }
+
+    public function saleItems(): HasMany
+    {
+        return $this->hasMany(SalesItem::class);
+    }
+}
